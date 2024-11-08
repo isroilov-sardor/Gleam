fc7d2d179b7f4c516bf8df8aaa7b686416912975
isroilov-sardor <notfalse01@gmail.com>
2024-11-08 21:36:33 +0500

    done not  fulll

--- js/main.js
+++ js/main.js
@@ -1,63 +1,7 @@
-window.addEventListener("load", function () {
-    setTimeout(function () {
-        document.querySelector(".loader").classList.add("hidden");
-    }, 0);
-});
-
-const button = document.querySelector("#button-log");
-const type1 = document.querySelector("#user-info");
-const type2 = document.querySelector("#password-info");
-const authButton = document.querySelector(".first-authitication");
-const authButton2 = document.querySelector(".second-authitication");
-const image = document.querySelector("#image");
-
-authButton &&
-    authButton.addEventListener("click", (event) => {
-        event.preventDefault();
-        alert(
-            "Ha'li beri bu qurulmalar ishlamayapti NOQULAYCHILIKLAR UCHUN UZUR"
-        );
-    });
-
-authButton2 &&
-    authButton2.addEventListener("click", (event) => {
-        event.preventDefault();
-        alert(
-            "Ha'li beri bu qurulmalar ishlamayapti NOQULAYCHILIKLAR UCHUN UZUR"
-        );
-    });
-
-const correctUsername = "admin";
-const correctPassword = "12345";
-
-button &&
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
-
-        if (!type1.value || !type2.value) {
-            alert("Iltimos, barcha maydonlarni to'ldiring!");
-            return;
-        }
-
-        if (
-            type1.value === correctUsername &&
-            type2.value === correctPassword
-        ) {
-            alert("parolni togri terdingiz MOLODES");
-            window.location.href = "./main.html";
-        } else {
-            alert("Username yoki Password noto'g'ri!");
-        }
-        type1.value = "";
-        type2.value = "";
-    });
-
-image &&
-    image.addEventListener("click", (event) => {
-        event.preventDefault();
-        if (type2.getAttribute("type") === "password") {
-            type2.setAttribute("type", "text");
-        } else {
-            type2.setAttribute("type", "password");
-        }
-    });
+function darkMode() {
+    const body = document.body;
+    const wasDark = localStorage.getItem("darkmode") === "true";
+    localStorage.setItem("darkmode", !wasDark);
+    body.classList.toggle("dark-mode", !wasDark);
+}
+document.querySelector("#btn").addEventListener("click", darkMode);
